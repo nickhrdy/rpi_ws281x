e577ffb980417ef6d46b0fdf047ae5ccb033bcf0
Nicolas Hardy <nickhrdy@vt.edu>
2021-02-04 22:37:49 -0500

    led: update driver

--- python/color_dict.py
+++ python/color_dict.py
@@ -1,8 +1,15 @@
+# dictionary of color mappings
+# vals can be any format recognized by the driver
+# e.g.,
+# #0000ff, #14031e44
+# [0, 0, 255]
+# [0, 32, 50, 0.4]
+# [0, 32, 50, 60]
 COLORS = {
     "blue": "#0000ff",
     "green": "#00ff00",
     "red": "#ff0000",
     "pale blue": "#0c21f0",    
-    "fuchsia": "#14031e",
+    "fuschia": "#14031e",
     "hot pink": "#320328",
 }
--- python/led.py
+++ python/led.py
@@ -21,65 +21,8 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 def eprint(*args):
+    """Print to stderr"""
     print(*args, file=sys.stderr)
-
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
-
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
-
-def rainbow(strip, wait_ms=20, iterations=1):
-    """Draw rainbow that fades across all pixels at once."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((i+j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-    """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def theaterChaseRainbow(strip, wait_ms=50):
-    """Rainbow movie theater light style chaser animation."""
-    for j in range(256):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, wheel((i+j) % 255))
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
-
 
 def pattern(strip):
     colors = [Color(255, 0, 0), Color(0, 255, 0), Color(0, 0, 255)]
@@ -88,15 +31,9 @@ def pattern(strip):
     strip.show()
 
 def single_color(strip, color):
-    """Draw rainbow that fades across all pixels at once."""
-    # think of 0-255 as affecting intensity as well
+    """Make the entire strip a single color"""
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
-    strip.show()
-
-def test_brightness(strip):
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, Color(0,0, strip.numPixels()//255*(strip.numPixels() - i)))
     strip.show()
 
 def apply_brightness(rgba):
@@ -105,13 +42,11 @@ def apply_brightness(rgba):
     Instead, a lower value is a dimmer color. Thus, we can decrease the r, g,
     and b values by a factor to simulate alpha.
     """
-    print(rgba)
     if type(rgba[3]) == float:
         factor = rgba[3]
     else:
         factor = rgba[3] / 255 
     ret = list(map(lambda x: int(x * factor), rgba[:3]))
-    print(ret)
     return ret
 
 def hex_to_rgb(hexcode):
@@ -121,7 +56,6 @@ def hex_to_rgb(hexcode):
     """
     if hexcode[0] == '#':
         hexcode = hexcode[1:]
-    print('incoming hex: ', hexcode)
     hexcode = int(hexcode, 16)
     rgba = [0xFF] * 4
     if hexcode & 0xFF000000:  # if there's a alpha value
@@ -133,43 +67,46 @@ def hex_to_rgb(hexcode):
             offset = 16 - (i*8)
             rgba[i] = (hexcode & (0xFF << offset)) >> offset
     # apply alpha to colors
-    return apply_brightness(rgba)
+    return rgba
 
-def handle_color_arg(strip, args):
+def handle_color_arg(args):
     """
     handle the color choice. The color choice can be:
     - a string representing the name of a color. This should be compared to the internal dictionary
     - 3 numbers representing an RGB value
     """
-    num_args = len(args)
     color = None
-    if num_args == 1:
-        color = Color(*hex_to_rgb(args[0])) 
+    num_args = len(args)
+    if type(args) == str:
+        color = hex_to_rgb(args) 
+    elif num_args == 1:
+        color = hex_to_rgb(args[0]) 
     elif num_args == 3:
-        color = Color(*map(int, args))
+        color = list(map(int, args))
+        color.append(1.0)
     elif num_args == 4:
-        color = Color(*apply_brightness(list(map(float,args))))
+        color = list(map(float,args))
     else:
         eprint('Number of arguments not recognized')
         exit(-1)
-    single_color(strip, color)
-
+    return color
 
 # Main program logic follows:
 if __name__ == '__main__':
     # Process arguments
     """
     options:
-        - choose a specific color
+        - (DONE) choose a specific color
         - choose a set of colors to repeat
         - option to fade between colors
         - option to wipe between colors
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-q', '--clear', action='store_true', help='clear the display on exit')
-    parser.add_argument('-c', '--color', nargs='+', help='specify a specific RGB color e.g. 255 255 255')
-    parser.add_argument('-C', '--name_color')
-    parser.add_argument('--off', action='store_true', help='turn lights off')
+    #parser.add_argument('-q', '--clear', action='store_true', help='clear the display on exit')
+    parser.add_argument('-c', '--color', nargs='+', help='specify a specific color. color formats can be RGB/RGBA or hex code (e.g., "255 0 0", "255, 0, 0, 0.5", "#fe3211", "#442ab308")')
+    parser.add_argument('-C', '--name_color', type=str, help='specify a color by name. A dictionary of available names can be found in color_dict.py')
+    parser.add_argument('-b', '--brightness', type=float, help='brightness of lights (0.0-1.0). This will overwrite any brightness value implies by the other flags')
+    parser.add_argument('-q', '--quit', action='store_true', help='turn lights off')
     args = parser.parse_args()
 
     # Create NeoPixel object with appropriate configuration.
@@ -177,20 +114,26 @@ if __name__ == '__main__':
     # Intialize the library (must be called once before other functions).
     strip.begin()
 
-    if args.off:
+    if args.quit:
         single_color(strip, Color(0, 0, 0))
         exit()
     try:
         if args.color:
-            handle_color_arg(strip, args.color)
+            rgba_vals = handle_color_arg(args.color)
         elif args.name_color:
-            if args.name_color not in COLORS:
-                eprint(f'color ${args[0]} not recognized')
+            key = args.name_color.lower()
+            if key not in COLORS:
+                eprint(f'color "{key}" not recognized')
                 exit()
-            single_color(strip, Color(*hex_to_rgb(COLORS[args.name_color])))
-        else:
-            # rainbow(strip, iterations=100)
-            test_brightness(strip)
+            print(COLORS[key])
+            rgba_vals = handle_color_arg(COLORS[key])
+
+        if args.brightness:
+            # overwrite current brightness value
+            rgba_vals[3] = args.brightness
+    
+        rgb_vals = apply_brightness(rgba_vals)
+        single_color(strip, Color(*rgb_vals))
 
     except KeyboardInterrupt:
         if args.clear:
